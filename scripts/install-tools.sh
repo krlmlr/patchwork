cfcd288b08ef168b43243f9f7017dd8d9e30b042
Kirill Müller <kirill@cynkra.com>
2026-04-06 18:11:17 +0200

    Append

--- scripts/install-tools.sh
+++ scripts/install-tools.sh
@@ -21,7 +21,7 @@ apt-get install -y --no-install-recommends \
 # ---------------------------------------------------------------------------
 # R package manager: pak (from Posit Package Manager binary repo)
 # ---------------------------------------------------------------------------
-echo 'options(repos = c(CRAN = sprintf("https://p3m.dev/cran/latest/bin/linux/noble-%s/%s", R.version["arch"], substr(getRversion(), 1, 3))))' | sudo tee /etc/R/Rprofile.site
+echo 'options(repos = c(CRAN = sprintf("https://p3m.dev/cran/latest/bin/linux/noble-%s/%s", R.version["arch"], substr(getRversion(), 1, 3))))' | sudo tee -a /etc/R/Rprofile.site
 Rscript -e 'install.packages("pak"); library(pak); pak::ppm_has_binaries()'
 
 # ---------------------------------------------------------------------------
